1831f03cd8dc1924a780f64ce830f9f185243808
Sumanth Shankar <careers.s.shankar@gmail.com>
2017-04-02 11:19:32 +0200

    adds unit testing

---
+++ src/rest-url-builder.service.spec.ts
@@ -0,0 +1,64 @@
+import { RestURLBuilder } from './rest-url-builder.service';
+
+describe('REST URL Builder', () => {
+  let restUrlBuilder: RestURLBuilder;
+  let testUrl = 'api.dev.talfinder.com/manageUser/:userId?hello=:hello&value=:value';
+
+  beforeEach(() => { restUrlBuilder = new RestURLBuilder(); });
+
+  it('should prepare rest url ', () => {
+    let expectedBaseURL = testUrl.substr(0, testUrl.indexOf('?'));
+    restUrlBuilder.buildRestURL(testUrl);
+    let baseURL = restUrlBuilder.getBaseURL();
+    expect(baseURL).toBe(expectedBaseURL);
+  });
+
+  it('should return given named parameters ', () => {
+    let expectedNamedParams = {
+      ':userId': null
+    }
+    restUrlBuilder.buildRestURL(testUrl);
+    let actualNamedParams = restUrlBuilder.getNamedParameters();
+    expect(actualNamedParams).toEqual(expectedNamedParams);
+  });
+
+  it('should return given query parameters', () => {
+    let expectedQueryParams = {
+      ':hello': null,
+      ':value': null
+    }
+    restUrlBuilder.buildRestURL(testUrl);
+    let actualQueryParams = restUrlBuilder.getQueryParameters();
+    expect(actualQueryParams).toEqual(expectedQueryParams);
+  })
+
+  it('should produce final url with named parameters and values only', () => {
+    let userValue = 'demo';
+    restUrlBuilder.buildRestURL(testUrl);
+    restUrlBuilder.setNamedParameter('userId', userValue);
+    let replacedValues = restUrlBuilder.get();
+    expect(replacedValues).toContain(userValue);
+    expect(replacedValues).not.toContain('hello=:hello');
+  });
+
+  it('should produce final url with named parameters/values and given queryParameter only', () => {
+    let userValue = 'demo';
+    let hello = 10;
+    restUrlBuilder.buildRestURL(testUrl);
+    restUrlBuilder.setNamedParameter('userId', userValue);
+    restUrlBuilder.setQueryParameter('hello', '' + 10);
+    let replacedValues = restUrlBuilder.get();
+    expect(replacedValues).toContain(userValue);
+    expect(replacedValues).toContain('?');
+    expect(replacedValues).toContain('hello=10');
+    expect(replacedValues).not.toContain('&');
+    expect(replacedValues).not.toContain('value=');
+  });
+
+  it('should have a base url when there are no query parameters', () => {
+    let urlWithoutQueryParams = 'api.dev.talfinder.com/companies/:companyId/assessments';
+    restUrlBuilder.buildRestURL(urlWithoutQueryParams);
+    let baseURL = restUrlBuilder.getBaseURL();
+    expect(baseURL).toBe(urlWithoutQueryParams);
+  });
+});
---
+++ src/rest-url-builder.service.ts
@@ -0,0 +1,103 @@
+export class RestURLBuilder {
+
+  private baseURL: string;
+  private namedParameters = {};
+  private queryParameters = {};
+  private firstQueryInvoked: boolean;
+  private parameterMatcher = /(:\b\D\w*)/g;
+
+  constructor() { }
+
+  buildRestURL(url: string): RestURLBuilder {
+    let baseURL = url.substr(0, url.indexOf('?'));
+    if (baseURL.length > 0) {
+      this.baseURL = baseURL;
+      let queryParamURL = url.substr(url.indexOf('?') + 1, url.length);
+      this.extractQueryParameters(queryParamURL);
+    } else {
+      this.baseURL = url;
+    }
+    this.extractNamedParameters(this.baseURL);
+    return this;
+  }
+
+  extractNamedParameters(url: string) {
+    let parameters = url.match(this.parameterMatcher);
+    if (parameters) {
+      parameters.forEach(value => this.namedParameters[value] = null);
+    }
+  }
+
+  extractQueryParameters(url: string) {
+    let parameters = url.match(this.parameterMatcher);
+    parameters.forEach(value => this.queryParameters[value] = null);
+  }
+
+  setNamedParameter(paramName: string, value: string) {
+    this.setParameter(paramName, value, this.namedParameters);
+  }
+
+  getNamedParameters() {
+    return this.namedParameters;
+  }
+
+  setQueryParameter(paramName: string, value: string) {
+    this.setParameter(paramName, value, this.queryParameters);
+  }
+
+  getQueryParameters() {
+    return this.queryParameters;
+  }
+
+  private setParameter(paramName: string, value: string, paramType: any) {
+    if (':' + paramName in paramType) {
+      paramType[':' + paramName] = value;
+      return this;
+    }
+    throw 'parameter ' + paramName + ' does not exist in the url: ' + this.baseURL;
+  }
+
+  get(): string {
+    let restURL = this.baseURL;
+    restURL = this.substitueNamedParameters(this.baseURL);
+    restURL = this.substitueQueryParameters(restURL);
+    return restURL;
+  }
+
+  private substitueNamedParameters(url: string): string {
+    return this.substitute(url, this.namedParameters, true, false);
+  }
+
+  private substitueQueryParameters(url: string) {
+    return this.substitute(url, this.queryParameters, false, true);
+  }
+
+  private substitute(url, parameters, substituteALL, isQueryParam) {
+    Object.keys(parameters)
+      .forEach(parameter => {
+        let value = parameters[parameter];
+        if (!value && substituteALL) {
+          throw new Error('The parameter ' + parameter + ' has not been set');
+        }
+
+        if (!isQueryParam) {
+          url = url.replace(parameter, value);
+        }
+
+        if (isQueryParam && value) {
+          if (!this.firstQueryInvoked) {
+            this.firstQueryInvoked = true;
+            url = url + '?' + parameter + '=' + value;
+          } else {
+            url = url + '&' + parameter + '=' + value;
+          }
+        }
+      });
+    return url;
+  }
+
+  getBaseURL(): string {
+    return this.baseURL;
+  }
+
+}
